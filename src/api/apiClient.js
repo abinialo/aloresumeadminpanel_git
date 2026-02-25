40b827a08604b4e4d58fdd4245cd44ce6774a05c
AbiniMA <abinialogroups@gmail.com>
2026-02-25 18:05:29 +0530

    console

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const apiBaseUrl = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api/v1';
 
+console.log("ENV VALUE:", process.env.REACT_APP_API_BASE_URL);
+console.log("API BASE URL USED:", apiBaseUrl);
 const apiClient = axios.create({
   baseURL: apiBaseUrl,
   headers: {
